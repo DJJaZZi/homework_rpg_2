b738f3538762b63f07695d600d4049fc31ce9bf8
DJJaZZi <siganak18@gmail.com>
2026-02-22 18:36:19 +0500

    IceComponentFactory created

---
+++ src/com/narxoz/rpg/factory/IceComponentFactory.java
@@ -0,0 +1,27 @@
+package com.narxoz.rpg.factory;
+
+import com.narxoz.rpg.combat.Ability;
+import com.narxoz.rpg.combat.FrostBreath;
+import com.narxoz.rpg.combat.IceShield;
+import com.narxoz.rpg.loot.LootTable;
+import com.narxoz.rpg.loot.IceLootTable;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class IceComponentFactory implements EnemyComponentFactory{
+    @Override
+    public List<Ability> createAbilities() {
+        return Arrays.asList(new FrostBreath(), new IceShield());
+    }
+
+    @Override
+    public LootTable createLootTable() {
+        return new IceLootTable();
+    }
+
+    @Override
+    public String createAIBehavior() {
+        return "DEFENSIVE";
+    }
+}
