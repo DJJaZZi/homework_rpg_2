e7fbfe509905e26f64eb2a3917c3a97fca390c33
DJJaZZi <siganak18@gmail.com>
2026-02-22 15:06:22 +0500

    FrostBreath and IceShield classes were modified

--- src/com/narxoz/rpg/combat/FrostBreath.java
+++ src/com/narxoz/rpg/combat/FrostBreath.java
@@ -1,4 +1,62 @@
 package com.narxoz.rpg.combat;
 
-public class FrostBreath {
+public class FrostBreath implements Ability{
+
+    private String name;
+    private int damage;
+    private String description;
+    private AbilityType type;
+
+    public FrostBreath(){
+        this.name = "Frost Breath";
+        this.damage = 75;
+        this.description = "Breathes a cone of freezing air, dealing damage and severely slowing enemies.";
+        this.type = AbilityType.Damage;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int getDamage() {
+        return damage;
+    }
+
+    @Override
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public AbilityType getType() {
+        return type;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setType(AbilityType type) {
+        this.type = type;
+    }
+
+    @Override
+    public Ability clone() {
+        FrostBreath copy = new FrostBreath();
+        copy.name = this.name;
+        copy.damage = this.damage;
+        copy.description = this.description;
+        copy.type = this.type;
+        return copy;
+    }
 }
--- src/com/narxoz/rpg/combat/IceShield.java
+++ src/com/narxoz/rpg/combat/IceShield.java
@@ -1,4 +1,73 @@
 package com.narxoz.rpg.combat;
 
-public class IceShield {
+public class IceShield implements Ability{
+
+    private String name;
+    private int damage;
+    private int defenseBoost;
+    private String description;
+    private AbilityType type;
+
+    public IceShield(){
+        this.name =  "Ice Shield";
+        this.damage = 0;
+        this.defenseBoost = 45;
+        this.description = "Encases the caster in thick ice, increasing defense by " + defenseBoost + " and absorbing physical blows.";
+        this.type = type;
+    }
+
+    public int getDefenseBoost(){
+        return defenseBoost;
+    }
+
+    @Override
+    public String getName() {
+        return "";
+    }
+
+    @Override
+    public int getDamage() {
+        return 0;
+    }
+
+    @Override
+    public String getDescription() {
+        return "";
+    }
+
+    @Override
+    public AbilityType getType() {
+        return null;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public void setDefenseBoost(int defenseBoost) {
+        this.defenseBoost = defenseBoost;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setType(AbilityType type) {
+        this.type = type;
+    }
+
+    @Override
+    public Ability clone() {
+        IceShield copy = new IceShield();
+        copy.name = this.name;
+        copy.damage = this.damage;
+        copy.defenseBoost = this.defenseBoost; // <--- Deep copy the stat boost
+        copy.description = this.description;
+        copy.type = this.type;
+        return copy;
+    }
 }
